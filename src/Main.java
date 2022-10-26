de099e8670def90b25c594bdb2c6f2a631d1f93f
Lyubsii <gordyuhina_lyuba@mail.ru>
2022-10-26 20:38:24 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        int ticketPrice = 3000;
+        int Mile = ticketPrice / 20;
+
+
+        System.out.println("Ваши бонусные мили: " + Mile);
+    }
+}
